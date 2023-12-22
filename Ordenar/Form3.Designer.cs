a259b12503f4c9bba6434baa71b691b00e8d615b
Juarez <coyote1972@outlook.com>
2023-12-22 14:13:09 -0300

    Ajustes

---
+++ Ordenar/Form3.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Ordenar
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            progressBar1 = new System.Windows.Forms.ProgressBar();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new System.Drawing.Point(9, 6);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new System.Drawing.Size(408, 23);
+            progressBar1.TabIndex = 0;
+            // 
+            // Form3
+            // 
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            ClientSize = new System.Drawing.Size(423, 37);
+            ControlBox = false;
+            Controls.Add(progressBar1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form3";
+            ShowInTaskbar = false;
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "0";
+            Load += Form3_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ Ordenar/Form3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ordenar
+{
+    public partial class Form3 : Form
+    {
+        private float _max;
+        private float _pb;
+
+        public float max
+        {
+            get
+            {
+                return _max;
+            }
+            set
+            {
+                _max = value;
+                progressBar1.Maximum = (int)_max;
+            }
+        }
+
+        public float pb
+        {
+            get
+            {
+                return _pb;
+            }
+            set
+            {
+                _pb = value;
+                progressBar1.Value = (int)_pb;
+                this.Text = ((int)_pb).ToString();
+            }
+        }
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            this.Text = "0";
+        }
+    }
+}
